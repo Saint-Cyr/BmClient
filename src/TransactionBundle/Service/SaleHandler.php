bac513dda23d2c6ea4a5c5a3f2a60b7223d65c67
Saint-Cyr <mapoukacyr@yahoo.fr>
2020-03-10 10:14:38 +0100

    update

--- src/TransactionBundle/Service/SaleHandler.php
+++ src/TransactionBundle/Service/SaleHandler.php
@@ -4,6 +4,11 @@ namespace TransactionBundle\Service;
 use TransactionBundle\Entity\STransaction;
 use TransactionBundle\Entity\Sale;
 use KmBundle\Entity\Branch;
+use Mike42\Escpos\Printer;
+use Mike42\Escpos\EscposImage;
+use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
+use Mike42\Escpos\PrintConnectors\FilePrintConnector;
+use TransactionBundle\Service\item;
 //use FOS\RestBundle\View\View;
 
 class SaleHandler
@@ -46,8 +51,102 @@ class SaleHandler
             $sale->setAmount($s['totalPrice']);
             $sale->setStransaction($stransaction);
             $this->em->persist($sale);
+			
+			$items [] = new item($product->getName()." #".$sale->getQuantity(),
+									 $sale->getQuantity()*$product->getUnitPrice()
+										 );
         }
-        //Generate idSynchrone here
+		
+		$subtotal = new item('Subtotal (No Tax)', $inputData['total'] - (($inputData['total']/100)*19));
+				$tax = new item('A local tax', '19%');
+				$total = new item('Total', $inputData['total'], true);
+		
+		//Start of TM-T20
+				try {
+				// Enter the share name for your USB printer here
+				//$connector = new WindowsPrintConnector("TM-T20");
+                                $connector = new FilePrintConnector("/dev/usb/lp0");
+				$printer = new Printer($connector);
+				
+				/* Information for the receipt */
+
+/* Date is kept the same for testing */
+ $date = date('l jS \of F Y h:i:s A');
+//$date = "Monday 6th of April 2015 02:56:25 PM";
+/* Start the printer */
+
+$logo = EscposImage::load(getcwd()."/images/logo_receipt.jpg", false);
+//$logo_company = EscposImage::load(getcwd()."/images/logo_company.png", false);
+$printer = new Printer($connector);
+
+/* Name of shop */
+$printer -> selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
+//$printer -> text("New World Telecom Cameroon Ltd.\n");
+$printer -> setJustification(Printer::JUSTIFY_CENTER);
+//$printer -> graphics($logo_company);
+$printer -> setDoubleStrike(false);
+$printer -> setBarcodeWidth(20);
+$printer -> setBarcodeHeight(30);
+//$printer -> barcode("987654321");
+$printer -> selectPrintMode();
+$printer -> feed(1);
+$printer -> text("SOCIETE HAMADJODA & FRERES SARL.\n");
+$printer -> text("Region Ouaka Ville de Bambari.\n");
+$printer -> text("Tel:+(236) 75 90 50 90 / 72 90 50 90\n");
+$printer -> feed();
+/* Title of receipt */
+$printer -> setEmphasis(true);
+$printer -> text("REÇU DE TRANSACION\n");
+$printer -> setEmphasis(false);
+/* Items */
+$printer -> setJustification(Printer::JUSTIFY_LEFT);
+$printer -> setEmphasis(true);
+$printer -> text(new item('', 'FCFA'));
+$printer -> setEmphasis(false);
+foreach ($items as $item) {
+    $printer -> text($item);
+}
+$printer -> setEmphasis(true);
+$printer -> text($subtotal);
+$printer -> setEmphasis(false);
+$printer -> feed();
+/* Tax and total */
+$printer -> text($tax);
+$printer -> selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
+$printer -> text($total);
+$printer -> selectPrintMode();
+/* Footer */
+$printer -> feed(1);
+$printer -> setJustification(Printer::JUSTIFY_CENTER);
+//$printer -> text("Caissier: ".$stransaction->getUser()->getName()."\n");
+$printer -> text("NB: Les articles vendus ne sont ni repris,  ni   rembourses\n");
+$printer -> text("Merci de nous avoir fait confiance.\n");
+//$printer -> text("For trading hours, please visit example.com\n");
+$printer -> feed(1);
+/* Print top logo */
+$printer -> text($date . "\n");
+$printer -> text("Caissier: Nouradine GONI\n");
+$printer -> pulse();
+$printer->qRcode("Nouradine GONI");
+//$printer->barcode("12345");
+//$printer->setBarcodeHeight(30);
+//$printer->setBarcodeWidth(10);
+//$priner->setBarcodeTextPosition();
+//$printer->pdf417Code("Saint");
+
+$printer -> setJustification(Printer::JUSTIFY_RIGHT);
+$printer -> graphics($logo);
+
+				/* Cut the receipt and open the cash drawer */
+				$printer -> cut();
+				
+				$printer -> close();
+				/* A wrapper to do organise item names & prices into columns */
+				} catch(Exception $e) {
+					echo "Couldn't print to this printer: " . $e -> getMessage() . "\n";
+				}
+				//End of TM-T20
+
         
         //Persist its in DB.
         $this->em->persist($stransaction);
